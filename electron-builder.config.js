3e910286d2609814a45e6291cd2ec083d16025e0
kokeehyuk-ship-it <kokeehyuk@gmail.com>
2026-05-14 14:21:03 +0900

    auto: 12h backup 2026-05-14 14:20

---
+++ electron-builder.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  appId: 'com.21nl.game',
+  productName: '21NL',
+  directories: { output: 'release' },
+  files: [
+    'main.js',
+    'index.html',
+    'src/**/*',
+    'assets/**/*',
+    'node_modules/phaser/dist/phaser.min.js',
+  ],
+  win: {
+    target: ['portable'],
+  },
+  portable: {
+    artifactName: '21NL_v${version}_portable.exe',
+  },
+};
